fb3d0c4c75c1042476ce5c45ca65cc0c50f1b7bd
SocratesDz <sdsxd1800@gmail.com>
2019-06-08 17:20:56 -0400

    Add infinite list of items.

--- lib/data/repositories/api.dart
+++ lib/data/repositories/api.dart
@@ -3,11 +3,24 @@ import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:http/http.dart' as http;
+import 'package:polandball/data/constants.dart';
 import 'package:polandball/domain/entities/models.dart';
 
 class RedditApi {
   final REDDIT_API = "https://www.reddit.com/";
   final SUBREDDIT = "r/polandball.json";
+
+  Future<Data> getPostsData({String after, String before}) async {
+    var uri = Uri.https(REDDIT_AUTHORITY, POLANDBALL_SUBREDDIT, {
+      "after": after,
+      "before": before
+    });
+    return http.get(uri.toString()).then((response) {
+      var responseBodyMap = json.decode(response.body);
+      var redditResponse = RedditResponse.fromJson(responseBodyMap);
+      return redditResponse.data;
+    });
+  }
 
   Future<List<Post>> getPosts() async {
     var url = REDDIT_API + SUBREDDIT;
