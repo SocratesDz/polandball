e6cd5ce3181253e552550b557637796f6568e749
SocratesDz <sdsxd1800@gmail.com>
2019-06-21 21:32:02 -0400

    Wiggly drawn shape border * Postponed implementation of ShapeBorder. * Use simple rounded black shape for post images.

--- lib/app/widgets/wiggly_shape_border.dart
+++ lib/app/widgets/wiggly_shape_border.dart
@@ -1,11 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class WigglyShapeBorder extends ShapeBorder {
   final double width;
 
-  const WigglyShapeBorder({
-    this.width: 1.0
-  }): assert(width != null);
+  const WigglyShapeBorder({this.width: 1.0}) : assert(width != null);
 
   @override
   EdgeInsetsGeometry get dimensions {
@@ -14,17 +13,27 @@ class WigglyShapeBorder extends ShapeBorder {
 
   @override
   void paint(Canvas canvas, Rect rect, {TextDirection textDirection}) {
-
+    final paint = new Paint()
+      ..color = Colors.black
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = width;
+    canvas.drawPath(getOuterPath(rect), paint);
   }
 
   @override
   Path getInnerPath(Rect rect, {TextDirection textDirection}) {
-
+    return new Path()
+      ..fillType = PathFillType.evenOdd
+      ..addPath(getOuterPath(rect), Offset.zero);
   }
 
   @override
   Path getOuterPath(Rect rect, {TextDirection textDirection}) {
-
+    return new Path()
+      ..lineTo(rect.right, rect.top)
+      ..lineTo(rect.width, rect.bottom)
+      ..lineTo(rect.left, rect.bottom)
+      ..lineTo(rect.left, rect.top);
   }
 
   @override
